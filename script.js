6606715e90c39bef4124f28842c94de49673056f
Benoit Junior <benoitjeanrobertjunior@gmail.com>
2025-05-16 21:57:20 +0000

    gestion du mode sombre

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// const h2 = document.createElement("h2");
-// h2.textContent = "This content added by JavaScript";
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const moonIcon = '<i class="fas fa-moon"></i>';
+const sunIcon = '<i class="fas fa-sun"></i>';
 
-// document.querySelector("body").appendChild(h2);
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark-mode');
+        if(darkModeToggle) darkModeToggle.innerHTML = sunIcon;
+    } else {
+         if(darkModeToggle) darkModeToggle.innerHTML = moonIcon;
+    }
+
+    if (darkModeToggle) {
+        darkModeToggle.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            if (body.classList.contains('dark-mode')) {
+                localStorage.setItem('darkMode', 'enabled');
+                darkModeToggle.innerHTML = sunIcon;
+            } else {
+                localStorage.setItem('darkMode', 'disabled');
+                darkModeToggle.innerHTML = moonIcon;
+            }
+        });
+    }
